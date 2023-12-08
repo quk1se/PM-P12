8a037fe5ee4d560f188f3c771d9a1296d5abd7d4
alexandr <KalX_si54@student.itstep.org>
2023-12-08 23:11:15 +0200

    final modifications

--- App/Helper.cs
+++ App/Helper.cs
@@ -9,41 +9,39 @@ namespace App
 {
     public class Helper
     {
-        public string HtmlTest(string input)
-        {
-            if (input is null)
-            {
-                throw new ArgumentException("Argument 'html' is null");
-            }
-            if (input.Length == 0)
-            {
-                return "";
-            }
-            input = input.Replace("<", "&lt;");
-            input = input.Replace(">", "&gt;");
-            return input;
-        }
-        public bool ContainsAttributes(String html)
-        {
-            string pattern = @"<\w+\s+[^=]*(\w+=[^>]+)+>";
-            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
-            return regex.IsMatch(html);
-        }
         public String Ellipsis(String input, int len)
         {
+            if (input == null)
+            {
+                throw new ArgumentNullException("null detected in parameter: " + nameof(input));
+            }
+            if (len < 3)
+            {
+                throw new ArgumentNullException("Argument 'len' could not be less than 3");
+            }
+            if (input.Length < len)
+            {
+                throw new ArgumentOutOfRangeException("Argument 'len' could not be greater than 'len'");
+            }
             // return "He...";
             // return (len == 5) ? "He..." : "Hel...";
             // return "Hel"[..(len-3)]+"...";
             return input[..(len - 3)] + "...";
         }
-        public string Dots(string input, int len)
+
+
+        //Method for adding a period at the end of a line
+        public string Dots(string input, int len) 
         {
             if (input[..(len - 1)] != ".")
                 return input[..(len - 1)] + ".";
             else
                 return input;
         }
-        public string CombineUrl(string part1, string part2)
+
+
+        //Method for concatenating strings correctly
+        public string CombineUrl(string part1, string part2) 
         {
             //if (!part1.StartsWith('/')) part1 = '/' + part1;
             //if (part1.EndsWith('/')) part1 = part1[..^1];
@@ -51,8 +49,6 @@ namespace App
             //if (!part2.StartsWith('/')) part2 = '/' + part2;
             //if (part2.EndsWith('/')) part2 = part2[..^1];
             //return $"{part1}{part2}";
-
-
             while (part1.StartsWith('/')) { part1 = part1.Substring(1); }
             part1 = '/' + part1;
             while (part1.EndsWith('/')) { part1 = part1[..^1]; }
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -46,23 +46,21 @@ namespace UnitTest
         {
             Helper helper = new();
             Assert.IsNotNull(helper, "new Helper() should not be null");
-            //Assert.AreEqual(
-            //    "He...",
-            //    helper.Ellipsis("Hello, World", 5));
-            //Assert.AreEqual(
-            //    "Hel...",
-            //    helper.Ellipsis("Hello, World", 6));
-            //Assert.AreEqual(
-            //    "Test...",
-            //    helper.Ellipsis("Test String", 7));
+            Assert.AreEqual(
+                "He...",
+                helper.Ellipsis("Hello, World", 5));
+            Assert.AreEqual(
+                "Hel...",
+                helper.Ellipsis("Hello, World", 6));
+            Assert.AreEqual(
+                "Test...",
+                helper.Ellipsis("Test String", 7));
 
-
-            //   DOTS TEST
-            //Assert.AreEqual("test.", helper.Dots("test", 5));
-            Assert.AreEqual("test", helper.Dots("test", 5));
         }
 
+
         [TestMethod]
+        //Test method to check if strings are concatenated correctly
         public void CombineUrlTest()
         {
             Helper helper = new();
@@ -71,15 +69,36 @@ namespace UnitTest
                 //{ new[] {"/home", "/index", "/123"}, "/home/index/123"},
                 //{ new[] {"/shop/", "/cart", "123/"}, "/shop/cart/123" },
                 //{ new[] {"auth/", "logout", "/123/" }, "/auth/logout/123"},
-                { new[] {"/home///", "index"}, "/home/index"},
-                { new[] { "///home/", "/index"}, "/home/index" },
-                { new[] { "home/", "////index"}, "/home/index"},
+                { new[] {"/home///", "index"}, "/home/index/null"},
+                { new[] { "///home/", "/index"}, "/home/index/null" },
+                { new[] { "home/", "////index"}, "/home/index/null"},
             };
             foreach (var testCase in testCases)
             {
                 Assert.AreEqual(testCase.Value, helper.CombineUrl(testCase.Key[0],
                     testCase.Key[1]), $"{testCase.Value} -- {testCase.Key[0]} + {testCase.Key[1]}");
             }
+        }
+
+        [TestMethod]
+        //Test method for checking matches in a modified string of words
+        public void EllipsisExceptionTest()
+        {
+            Helper helper = new();
+            var ex = Assert.ThrowsException<ArgumentNullException>( () => helper.Ellipsis(null!, 1));
+            Assert.IsTrue(
+                ex.Message.Contains("input"),
+                "Exception message shoud contain 'input' substring"
+                );
+            var ex2 = Assert.ThrowsException<ArgumentException>(
+                         () => helper.Ellipsis("2342344", 2)
+                     );
+            Assert.IsTrue(ex2.Message.Contains("len"));
+
+            var ex3 = Assert.ThrowsException<ArgumentOutOfRangeException>(
+                         () => helper.Ellipsis("1234", 6)
+                     );
+            Assert.IsTrue(ex3.Message.Contains("len"));
         }
     }
 }
